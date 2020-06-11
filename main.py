320e1141d1831fd91acceb5d98b013bd2cc19b0f
Syn483 <twocap06@gmail.com>
2020-06-11 18:26:53 +0100

    fixing bugs

--- main.py
+++ main.py
@@ -22,24 +22,38 @@ def unicode_to_ascii(s):
 
 def normalize_string(s):
     s = unicode_to_ascii(s.lower().strip())
-    s = re.sub(r"([.!?])", r"\1", s)
-    s = re.sub(r"[^a-zA-Z.!?]+", r"", s)
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
     return s
 
-def read_data(lang_1, lang_2, reversed=False):
+def read_data(lang_1, lang_2, reverse=False):
     print("importing dataset")
-    with open("data/{}-{}.txt".format(lang_2, lang_1), "r", encoding="utf-8").read() as f:
-        pairs = [[normalize_string(s) for s in l.split("\t")] for l in f.split("\n")]
+    f = open("data/{}-{}.txt".format(lang_1, lang_2), "r", encoding="utf-8").read().strip()
+    pairs = [[normalize_string(s) for s in l.split("\t")] for l in f.split("\n")]
 
-        if reversed:
-            pairs = [list(reversed(p)) for p in pairs]
-            input_lang = Lang(lang_2)
-            output_lang = Lang(lang_1)
-        else:
-            input_lang = Lang(lang_1)
-            output_lang = Lang(lang_2)
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang_2)
+        output_lang = Lang(lang_1)
+    else:
+        input_lang = Lang(lang_1)
+        output_lang = Lang(lang_2)
         
-        return input_lang, output_lang, pairs
+    return input_lang, output_lang, pairs
 
+def prepare_data(lang_1, lang_2, reverse=False):
+    input_lang, output_lang, pairs = read_data(lang_1, lang_2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    print("Counting words...")
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
 
+input_lang, output_lang, pairs = prepare_data('eng', 'fra', True)
+print(random.choice(pairs))
         
